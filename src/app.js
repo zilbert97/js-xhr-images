34dda5bf0eee02c39a2013c392a09010c4c82b85
Zach Gilbert <gilberza@gmail.com>
2021-03-25 09:46:05 +0000

    Added responsive buttons and input field

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!!!');
+const buttons = document.getElementsByClassName('gallery--item-remove');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', (e) => {
+
+    let wrapper = e.target.parentNode.parentNode;
+    wrapper.parentNode.removeChild(wrapper);
+
+  });
+}
