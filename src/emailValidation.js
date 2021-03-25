452abed3a07c145920971dd7aa9e9c01249afa17
Zach Gilbert <gilberza@gmail.com>
2021-03-25 11:15:58 +0000

    Added message displayed on front end as a result of form validation

--- src/emailValidation.js
+++ src/emailValidation.js
@@ -4,14 +4,30 @@ function createValidator() {
     const validate = input => {
         const reEmail = new RegExp('[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}');
 
-        if (reEmail.test(input.value.toUpperCase())) {
-            console.log('VALID');
+        if (!input.value) {
+            return 'empty';
+        }
+        else if (reEmail.test(input.value.toUpperCase())) {
+            return input.value;
         } else {
-            console.log('INVALID');
+            return false;
         }
     }
 
     return document.querySelector('.email--button').addEventListener('click', () => {
-        validate(emailInput);
+        const responseMessage = document.querySelector('.email--validation-copy');
+        const validEmail = validate(emailInput);
+
+
+        if (validEmail === 'empty') {
+          responseMessage.style.color = null;
+          responseMessage.innerHTML = 'Please provide an email address.';
+        } else if (validEmail) {
+          responseMessage.style.color = '#25BD38';
+          responseMessage.innerHTML = `Using ${validEmail}`;
+        } else {
+          responseMessage.style.color = null;
+          responseMessage.innerHTML = 'Invalid email address.'
+        }
     });
 }
