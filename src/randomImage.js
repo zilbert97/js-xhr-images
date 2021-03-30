f0116e5f74deafa37dd0b4cd5a17fbf4bdeb8a29
Zach Gilbert <gilberza@gmail.com>
2021-03-30 13:53:39 +0100

    Variable renaming

--- src/randomImage.js
+++ src/randomImage.js
@@ -24,7 +24,6 @@
 
     axios.get(`https://picsum.photos/v2/list?limit=250`).then(
         (response) => {
-            const imagePreview = document.querySelector('.image');
             const imageAuthor = document.querySelector('#meta-label--author > span');
             const imageSize = document.querySelector('#meta-label--size > span');
 
@@ -54,7 +53,7 @@
              * in the user instance passed.
              *
              * @param {array} data - All image objects.
-             * @return {void} None.
+             * @return {string} - The URL for the image loaded.
              */
             const setRandomImage = data => {
                 const imageObject = getRandomImageObject(data);
@@ -65,42 +64,37 @@
                 return imageObject.download_url;
             }
 
-            imagePreview.src = setRandomImage(response.data);
+            document.querySelector('.image').src = setRandomImage(response.data);
 
             // On reset button click gets a new image.
             document.querySelector('.image--reset-button').addEventListener(
                 'click', () => {
-                    let imagePreview = document.querySelector('.image');
+                    let previousImage = document.querySelector('.image');
 
-                    imagePreview.classList.toggle('fade-in');
-                    imagePreview.classList.toggle('fade-out');
+                    previousImage.classList.toggle('fade-in');
+                    previousImage.classList.toggle('fade-out');
 
                     // Set the image to hidden after fade-out, then get a new image
                     setTimeout(() => {
-                        imagePreview.style.visibility = 'hidden';
-                        imagePreview.classList.toggle('fade-out');
+                        previousImage.style.visibility = 'hidden';
+                        previousImage.classList.toggle('fade-out');
 
-                        // REPLACE THE ELEMENT WITH A NEW ELEMENT
+                        // Create a new image element
                         const newImage = new Image();
+                        // Set it to hidden (before included in the DOM)
                         newImage.style.visibility = 'hidden';
                         newImage.classList.add('image');
-                        newImage.onload = () => {
-                            console.log('LOADED');
-                        }
+                        // Set the src attribute to a random image URL
                         newImage.src = setRandomImage(response.data);
 
                         setTimeout(() => {
-                            imagePreview.parentNode.replaceChild(newImage, imagePreview);
+                            // Replace the image in the DOM with the new image
+                            previousImage.parentNode.replaceChild(newImage, previousImage);
+                            // Set the new image's visibility and fade-in
                             newImage.style.visibility = 'visible';
                             newImage.classList.toggle('fade-in');
                         }, 300);
                     }, 300);
-
-                    // Set the new image's visibility to visible then fade in,
-                    // after the previous image has been faded out and hidden.
-
-
-
                 }
             );
         }
